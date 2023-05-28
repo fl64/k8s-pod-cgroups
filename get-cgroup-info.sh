5bd34b7f9cdb81868eee968a7f6639dd0ed11d64
fl64 <flsixtyfour@gmail.com>
2023-05-28 22:08:10 +0300

    some fixes

--- get-cgroup-info.sh
+++ get-cgroup-info.sh
@@ -7,6 +7,9 @@ PROBER_NS="cgroup-prober"
 PROBER_LABELS="app=cgroup-prober"
 CGROUP_PATTERN="cpu"
 
+PROCSYS_PATH="/host"
+#PROCSYS_PATH=""
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -29,6 +32,7 @@ main() {
   echo
 
   podNodeName=$(kubectl -n "${1}" get pod "${2}" -o json | jq -r '.spec.nodeName')
+  # echo $podNodeName
   proberPod=${cgroupProber[${podNodeName}]}
 
   echo -e "${GREEN}Discover containers cgroups for pod:${NC} ${2} ${GREEN}in namespace:${NC} ${1} ${GREEN}on node:${NC} ${podNodeName} ${GREEN}via pod:${NC} ${proberPod}"
@@ -38,16 +42,24 @@ main() {
     containerID=$(echo "${container}" | jq -r '.containerID')
     echo -e "${GREEN}  Container:${NC} ${containerName}${GREEN}, containerd ID:${NC} ${containerID}"
 
-    processPID=$(proberExec "${proberPod}" "crictl inspect ${containerID} | jq .info.pid")
+    processPID=$(proberExec "${proberPod}" "crictl inspect ${containerID} | jq -r .info.pid")
     echo -e "${GREEN}    Process PID:${NC} ${processPID}"
 
-    processCgroups=$(proberExec "${proberPod}" "cat /host/proc/${processPID}/cgroup ")
-    # echo "Process CGroups: ${processCgroups}"
+    cgroupPath=$(proberExec "${proberPod}" "crictl inspect ${containerID} | jq -r .info.runtimeSpec.linux.cgroupsPath")
+    echo -e "${GREEN}    Process cgroupPath:${NC} ${cgroupPath}"
 
+    processCgroups=$(proberExec "${proberPod}" "cat ${PROCSYS_PATH}/proc/${processPID}/cgroup ")
+    echo "${processCgroups}" | sed 's/^/    - /'
+
+    echo -e "    ${GREEN}Process CGroups:${NC}"
     while IFS=':' read -r _ cgroup path; do
+      # little hack to remove /../../.. in some cases
+      echo $path | grep -q '/../../..' && path="${cgroupPath}"
+
       echo -e "${GREEN}      cgroup:${NC} ${cgroup} ${GREEN}path:${NC} ${path}"
-      # cur path from cgroup than add 4 spaces before
-      proberExec "${proberPod}" "grep '' /host/sys/fs/cgroup/${cgroup}${path}/* | rev | cut -d'/' -f1 | rev" | sed 's/^/        /' | grep -v cpuacct
+      # cut path from cgroup than add 4 spaces before
+      proberExec "${proberPod}" "grep '' ${PROCSYS_PATH}/sys/fs/cgroup/${cgroup}${path}/* | rev | cut -d'/' -f1 | rev" | sed 's/^/        /' | grep "${CGROUP_PATTERN}"
+      #proberExec "${proberPod}" "grep '' ${PROCSYS_PATH}/sys/fs/cgroup/${cgroup}${path}/*" | sed 's/^/        /'
     done < <(echo "${processCgroups}")
 
   done < <(kubectl -n "${1}" get pods "${2}" -o json | jq '.status.containerStatuses[] | {name, containerID: .containerID | split("://")[1] }' | jq -sc .[])
