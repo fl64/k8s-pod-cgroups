a5493be831d59e6ef245c31618c1532586211025
fl64 <flsixtyfour@gmail.com>
2023-05-28 18:28:53 +0300

    refactor

--- get-cgroup-info.sh
+++ get-cgroup-info.sh
@@ -5,6 +5,7 @@
 
 PROBER_NS="cgroup-prober"
 PROBER_LABELS="app=cgroup-prober"
+CGROUP_PATTERN="cpu"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -21,30 +22,31 @@ proberExec() {
 echo -e "${GREEN}Prober pods:${NC}"
 declare -A cgroupProber
 while IFS=' ' read -r podName nodeName; do
-    echo pod: $podName node: $nodeName
-    cgroupProber[$nodeName]=$podName
+    echo -e "  ${GREEN}pod:${NC} ${podName} ${GREEN}node:${NC} ${nodeName}"
+    cgroupProber[$nodeName]="${podName}"
 done < <(kubectl -n "${PROBER_NS}" get pods -l "${PROBER_LABELS}" -o custom-columns=NAME:.metadata.name,NODE:.spec.nodeName --no-headers)
 echo
 
 podNodeName=$(kubectl -n "${1}" get pod "${2}" -o json | jq -r '.spec.nodeName')
 proberPod=${cgroupProber[${podNodeName}]}
 
-echo -e "${GREEN}Discover containers cgroups for pod:${NC} ${2} ${GREEN}in namespace${NC} ${1} ${GREEN}on node:${NC} ${podNodeName} ${GREEN}via pod:${NC} ${proberPod}"
+echo -e "${GREEN}Discover containers cgroups for pod:${NC} ${2} ${GREEN}in namespace:${NC} ${1} ${GREEN}on node:${NC} ${podNodeName} ${GREEN}via pod:${NC} ${proberPod}"
 while read container; do
 
   containerName=$(echo "${container}" | jq -r '.name')
   containerID=$(echo "${container}" | jq -r '.containerID')
-  echo -e "${GREEN}Container:${NC} ${containerName}${GREEN}, containerd ID:${NC} ${containerID}"
+  echo -e "${GREEN}  Container:${NC} ${containerName}${GREEN}, containerd ID:${NC} ${containerID}"
 
   processPID=$(proberExec "${proberPod}" "crictl inspect ${containerID} | jq .info.pid")
-  echo -e "${GREEN}Process PID:${NC} ${processPID}"
+  echo -e "${GREEN}    Process PID:${NC} ${processPID}"
 
-  processCgroups=$(proberExec "${proberPod}" "cat /host/proc/${processPID}/cgroup | grep cpu")
+  processCgroups=$(proberExec "${proberPod}" "cat /host/proc/${processPID}/cgroup | grep ${CGROUP_PATTERN}")
   #echo "Process CGroups: ${processCgroups}"
 
-  while IFS=':' read -r num cgroup path; do
-    echo -e "${GREEN}cgroup:${NC} ${cgroup} ${GREEN}path:${NC} ${path}"
-    proberExec "${proberPod}" "grep '' /host/sys/fs/cgroup/${cgroup}${path}/* | rev | cut -d'/' -f1 | rev" | sed 's/^/    /'
+  while IFS=':' read -r _ cgroup path; do
+    echo -e "${GREEN}      cgroup:${NC} ${cgroup} ${GREEN}path:${NC} ${path}"
+    # cur path from cgroup than add 4 spaces before
+    proberExec "${proberPod}" "grep '' /host/sys/fs/cgroup/${cgroup}${path}/* | rev | cut -d'/' -f1 | rev" | sed 's/^/        /' | grep -v cpuacct
   done < <(echo "${processCgroups}")
 
 done < <(kubectl -n "${1}" get pods "${2}" -o json | jq '.status.containerStatuses[] | {name, containerID: .containerID | split("://")[1] }' | jq -sc .[])
